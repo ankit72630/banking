85c7e81dc607b85c72896c4e7cd36863544eb2ce
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:34:51 +0530

    Update cc-activation-success.tsx

--- src/modules/dashboard/thank-you/cc-activation-success.tsx
+++ src/modules/dashboard/thank-you/cc-activation-success.tsx
@@ -1,1 +1,55 @@
+import "./thank-you.scss";
+import { KeyWithAnyModel } from "../../../utils/model/common-model";
+import ThankYouTimeline from "./thankyou-timeline";
+import ThankYouBanner from "./thankyou-banner";
+
+const CCActivationSucess = (props: KeyWithAnyModel) => {
+  const applicationDetails = props.applicationDetails;
+  const thankyou = props.thankyou;
+  return (
+    <>
+      <ThankYouBanner
+        banner_header={thankyou.CCActivation.banner.banner_header}
+        banner_content={false}
+      />
+      <div className="thankyou__body__outer">
+        <div className="thankyou__body">
+          <div className="thankyou__title">
+            {thankyou.CCActivation.header}
+          </div>
+          <div className="thankyou__title">
+            <div>{applicationDetails.productName}</div>
+            <div>
+              <label>{applicationDetails.cardNumber}</label>
+            </div>
+          </div>
+          <ThankYouTimeline
+            title={thankyou.CCActivation.timeline_header}
+            data={thankyou.CCActivation.successTimeLine}
+            checkCompletedStatus={true}
+          />
+          <div className="body__notes">
+            <div className="body__notes__desc">
+              {thankyou.CCActivation.successNote}
+             </div> 
+          </div>                
+          <div className="body__app-details">
+            <label>{thankyou.CCPL.refId_lbl}</label>
+            {props.applicationReferenceNo!}
+          </div>
+          <div className="body__refno">
+            <button
+              onClick={(e) => props.goToIBanking(e)}
+              className="thankyou__continue"
+            >
+              {thankyou[applicationDetails.thankyouText].iBankingButton}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CCActivationSucess;
 

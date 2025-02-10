73c5a6959b340af58dafb49d32deeefe5a145afb
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 09:16:00 +0530

    Update constant.ts

--- src/shared/components/alias/constant.ts
+++ src/shared/components/alias/constant.ts
@@ -1,1 +1,8 @@
-
+export const constant = {
+    type: "button",
+    name: "Add another alias/Previous Names",
+    ariaLabel: "Add another alias/Previous Names",
+    id: "add-new-alias-button",
+    placeholder: "Add new alias",
+    value: "Add another alias/Previous Names"
+}

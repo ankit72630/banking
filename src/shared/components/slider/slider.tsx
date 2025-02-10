398d4ec4fc41191aa2d7cbaabb2480a63ef071d8
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 11:04:21 +0530

    Update slider.tsx

--- src/shared/components/slider/slider.tsx
+++ src/shared/components/slider/slider.tsx
@@ -1,1 +1,25 @@
+import { KeyWithAnyModel } from "../../../utils/model/common-model";
+import "./slider.scss";
+import RcSlider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+const Slider = (props: KeyWithAnyModel) => {
+  return (
+    <>
+      <div className ="rc-slider-container">
+        <RcSlider
+          step={props.options.step}
+          min={props.options.min}
+          max={props.options.max}
+          marks={props.options.marks}
+          dots={props.options.dots}
+          value = {props.value}
+          onChange={value => props.updateSliderValue(`${value}`)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Slider;
 

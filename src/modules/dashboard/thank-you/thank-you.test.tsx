8a1fa9696e59d34a787b68798bf0a70a9b0102e1
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:33:22 +0530

    Update thank-you.test.tsx

--- src/modules/dashboard/thank-you/thank-you.test.tsx
+++ src/modules/dashboard/thank-you/thank-you.test.tsx
@@ -1,1 +1,33 @@
+import {Provider} from 'react-redux'
+import { shallow } from 'enzyme'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
+import storeMockData from './../../../utils/mock/store-spec.json';
+import ThankYou from '../thank-you/thank-you';
 
+jest.autoMockOff();
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+jest.mock("@lottiefiles/react-lottie-player", () => ({
+  __esModule: true,
+}));
+
+const mockStore = configureMockStore([thunk]);
+let wrapper:any;
+let store: any;
+beforeEach(() => {
+    store = mockStore(storeMockData);
+    wrapper = shallow(
+        <Provider store={store}>
+          <ThankYou />
+        </Provider>
+      );
+  });
+
+
+describe('Thank you screen', () => {
+  it('should render thank you component', () => {
+    expect(wrapper.find('.thankyou').length).toEqual(0)
+  })
+})

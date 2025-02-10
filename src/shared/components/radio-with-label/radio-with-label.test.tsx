35c2e8b77359e383b7235b35a34b6c7933a3a508
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:58:42 +0530

    Update radio-with-label.test.tsx

--- src/shared/components/radio-with-label/radio-with-label.test.tsx
+++ src/shared/components/radio-with-label/radio-with-label.test.tsx
@@ -1,1 +1,31 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import RadioWithLabel from "./radio-with-label";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+jest.mock("@lottiefiles/react-lottie-player", () => ({
+  __esModule: true,
+}));
+
+describe("RadioWithLabel Model Component Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("RadioWithLabel Model component render correctly", () => {
+        store = mockStore({name : "aaaa"});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <RadioWithLabel/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(RadioWithLabel)).toHaveLength(1);
+      });
+});

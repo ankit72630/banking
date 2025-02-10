b599248aefad6a6c81bebedb654a8a584edfb10b
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:54:10 +0530

    Update popup-model.tsx

--- src/shared/components/popup-modal/popup-model.tsx
+++ src/shared/components/popup-modal/popup-model.tsx
@@ -1,1 +1,21 @@
+import React from "react";
+import "./popup-model.scss";
+
+const PopupModel = (props: any) => {
+  return (
+    <>
+      {props.displayPopup && (
+        <div
+          className={`${
+            props.timeoutClass ? props.timeoutClass : ""
+          } popup-container`}
+        >
+          {props.children}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PopupModel;
 

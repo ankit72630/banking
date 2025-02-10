dbae6423ee1cbf05b5f91ea6dd6d695a299d98c9
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 07:31:18 +0530

    Update static-screen.tsx

--- src/modules/dashboard/fields/static-screen.tsx
+++ src/modules/dashboard/fields/static-screen.tsx
@@ -1,1 +1,28 @@
+import { useEffect } from "react";
+import Fields from "./fields";
+import { stateUrl } from "./stage.utils";
+
+const StaticScreens = (props: any) => {
+
+  useEffect(() => {
+    stateUrl(props.pageStage);
+  }, [props.pageStage]);
+  
+  return (
+    <>
+      <div className="static-screens">
+        {(() => {
+          switch (props.pageStage) {
+            default:
+              return <Fields />;
+          }
+        })()}
+      </div>
+    </>
+  );
+};
+
+export default StaticScreens;
+
+
 

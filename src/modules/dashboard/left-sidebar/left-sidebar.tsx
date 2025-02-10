d15489cf469e6b53a1f7435c346eef451291ecfd
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:16:06 +0530

    Update left-sidebar.tsx

--- src/modules/dashboard/left-sidebar/left-sidebar.tsx
+++ src/modules/dashboard/left-sidebar/left-sidebar.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import "./left-sidebar.scss";
+
+const LeftSideBar = () => {
+  return (
+    <>
+      <div className="left-menu">
+        <div className="container">
+          <div className="logo-background">
+            <div className="container__logo"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LeftSideBar;
 

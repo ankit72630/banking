981fdc856d63ba4dff11e0d8354dcc74cb31e866
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:34:31 +0530

    Update cc-without-actvation.tsx

--- src/modules/dashboard/thank-you/cc-without-actvation.tsx
+++ src/modules/dashboard/thank-you/cc-without-actvation.tsx
@@ -1,1 +1,63 @@
+import "./thank-you.scss";
+import { KeyWithAnyModel } from "../../../utils/model/common-model";
+import ThankYouTimeline from "./thankyou-timeline";
+import ThankYouBanner from "./thankyou-banner";
+
+const CCWithoutActivation = (props: KeyWithAnyModel) => {
+  const applicationDetails = props.applicationDetails;
+  const thankyou = props.thankyou;
+  return (
+    <>
+      <ThankYouBanner
+        banner_header={thankyou.CCActivation.banner.banner_header}
+        banner_content={false}
+      />
+      <div className="thankyou__body__outer">
+        <div className="thankyou__body">
+          <div className="thankyou__title">
+            {thankyou.CCActivation.banner.content_header}
+          </div>
+          <div className="thankyou__title">
+            <div>{applicationDetails.productName}</div>
+            <div>
+              <label>{applicationDetails.cardNumber}</label>
+            </div>
+          </div>
+          <ThankYouTimeline
+            title={thankyou[applicationDetails.thankyouText].timeLine}
+            data={thankyou.CCActivation.timeLine}
+            checkCompletedStatus={true}
+          />
+          <div className="body__notes">
+            <div className="body__notes__desc">
+              {thankyou.CCActivation.feedback.content_1}
+              <a
+                target="_blank"
+                rel="feedback noreferrer"
+                href={applicationDetails.feedbackUrl}
+              >
+                {thankyou.CCActivation.feedback.content_2}
+              </a>{" "}
+              {thankyou.CCActivation.feedback.content_3}
+            </div>
+          </div>
+          <div className="body__app-details">
+            <label>{thankyou.CCPL.refId_lbl}</label>
+            {props.applicationReferenceNo!}
+          </div>
+          <div className="body__refno">
+            <button
+              onClick={(e) => props.goToIBanking(e)}
+              className="thankyou__continue"
+            >
+              {thankyou[applicationDetails.thankyouText].iBankingButton}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CCWithoutActivation;
 

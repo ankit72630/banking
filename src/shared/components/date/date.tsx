aa38ebcf714553031e5cf8e730d1aff124dba794
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:13:18 +0530

    Update date.tsx

--- src/shared/components/date/date.tsx
+++ src/shared/components/date/date.tsx
@@ -1,1 +1,28 @@
 
+import { KeyWithAnyModel } from "../../../utils/model/common-model";
+
+import "./dates.scss";
+
+ 
+
+export const Date = (props: KeyWithAnyModel) => {
+
+ 
+
+  return (
+
+    <>
+
+      <div>Dates</div>
+
+    </>
+
+  );
+
+};
+
+ 
+
+export default Date;
+
+ 

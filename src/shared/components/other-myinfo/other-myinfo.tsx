7838f6e8236534a7faa7f1086eb050ee6fede20a
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:43:59 +0530

    Update other-myinfo.tsx

--- src/shared/components/other-myinfo/other-myinfo.tsx
+++ src/shared/components/other-myinfo/other-myinfo.tsx
@@ -1,1 +1,22 @@
+import { KeyWithAnyModel } from "../../../utils/model/common-model";
+import "./other-myinfo.scss";
+import trackEvents from "../../../services/track-events";
+
+const OtherMyinfo = (props: KeyWithAnyModel) => {
+  const showHandler = (event:any) => {
+    trackEvents.triggerAdobeEvent('ctaClick', 'See Other Myinfo Details');    
+    props.handleFieldDispatch(props.data.logical_field_name, "ssf-2", event);
+  };
+
+  return (
+    <div className="other-info">
+      <div className="other-info__inner">
+        <div onClick={showHandler}>{props.data.rwb_label_name}</div>
+        <div className="chevron"></div>
+      </div>
+    </div>
+  );
+};
+
+export default OtherMyinfo;
 

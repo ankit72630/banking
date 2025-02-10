20d04602739d13d84af972fd22883c5ae73906ed
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:43:31 +0530

    Update rules_bd-2.ts

--- src/modules/rules/rules_bd-2.ts
+++ src/modules/rules/rules_bd-2.ts
@@ -1,1 +1,32 @@
+import { checkProductDetails } from '../../services/common-service';
+import { getUrl } from '../../utils/common/change.utils';
+import { KeyWithAnyModel, ValidationObjModel } from '../../utils/model/common-model';
+import rulesUtils from './rules.utils';
+
+const Rules_bd_2 = (props: KeyWithAnyModel, stageInfo: KeyWithAnyModel): KeyWithAnyModel => {
+    const validationObj: ValidationObjModel = {
+        nonEditable: [],
+        hidden: [],
+        modifyVisibility:[]
+    };
+    const auth = getUrl.getParameterByName("auth");
+    const isCASAProduct = checkProductDetails(stageInfo.products);
+    let defaultVisiblity:any= []
+    if(auth === "manual"){
+        let hiddenFields = ["postal_code_other","country"]
+        if(!isCASAProduct){
+            hiddenFields.push('nationality_add');
+        }
+
+        if(stageInfo.applicants['residency_status_a_1'] === "FR"){
+            defaultVisiblity = ["overseas_contact_country_code","overseas_contact_area_code","overseas_contact_no"];
+        }
+        validationObj.hidden!.push(hiddenFields);
+        validationObj.modifyVisibility!.push(defaultVisiblity)
+      }
+
+    return rulesUtils(props, validationObj);
+}
+
+export default Rules_bd_2;
 

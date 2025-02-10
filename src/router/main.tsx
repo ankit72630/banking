ea5fe0d03059656be6a0e5387820d6552941f9ab
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 07:02:59 +0530

    Update main.tsx

--- src/router/main.tsx
+++ src/router/main.tsx
@@ -1,1 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Dashboard from "../modules/dashboard/dashboard";
+import ThankYou from "../modules/dashboard/thank-you/thank-you";
+import Otp from "../shared/components/otp-modal/otp";
+import PendingApplication from "../modules/dashboard/pending-resume-application/pending-resume-application";
+
+const Main = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="super-short-form" element={<Dashboard />}></Route>
+        <Route path="sg/thankyou" element={<ThankYou />}></Route>
+        <Route path="otp" element={<Otp />}></Route>
+        <Route path="pending-application" element={<PendingApplication />}></Route>
+        <Route path="/*" element={<Dashboard />}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default Main;
 

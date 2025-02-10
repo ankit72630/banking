347cb4f8b95ef9b9e3fe20dbbe7dd83f2067ad14
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 09:53:45 +0530

    Update cards.test.tsx

--- src/shared/components/cards/cards.test.tsx
+++ src/shared/components/cards/cards.test.tsx
@@ -1,1 +1,28 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import Cards from "./cards";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+
+describe("Cards Component Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("Cards component render correctly", () => {
+        store = mockStore({});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <Cards/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(Cards)).toHaveLength(1);
+      });
+});

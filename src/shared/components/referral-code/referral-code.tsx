2f9dc2ba7362001187daa828b06e5cc94ad7fc5f
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 11:00:36 +0530

    Update referral-code.tsx

--- src/shared/components/referral-code/referral-code.tsx
+++ src/shared/components/referral-code/referral-code.tsx
@@ -1,1 +1,19 @@
+import './referral-code.scss';
+const ReferralCode =()=>{
+    return(
+        <>
+        <div className='referral-code'>
+            <div className='referral-content-parent'>
+            <div className='referral-code-content'>
+                
+                <div className='referral-content-text'>Referred by a friend?</div>
+                <div className='referral-text-content'>If you’ve received a referral code,please enter below.</div>
+            </div>
+            <div className='referral-content-image'></div>
+            </div>
+        </div>
+        </>
+    )
+}
 
+export default ReferralCode;

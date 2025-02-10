743c29d033467b9456b186d8363c3a7eb747eab7
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:30:14 +0530

    Update model.test.tsx

--- src/shared/components/model/model.test.tsx
+++ src/shared/components/model/model.test.tsx
@@ -1,1 +1,31 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import Model from "./model";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+jest.mock("@lottiefiles/react-lottie-player", () => ({
+  __esModule: true,
+}));
+
+describe("Model Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("Model component render correctly", () => {
+        store = mockStore({});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <Model/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(Model)).toHaveLength(1);
+      });
+});

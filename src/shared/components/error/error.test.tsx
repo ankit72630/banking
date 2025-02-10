3df76e9cbed16243e6cc5bfd1b05ff44075a102f
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:20:41 +0530

    Update error.test.tsx

--- src/shared/components/error/error.test.tsx
+++ src/shared/components/error/error.test.tsx
@@ -1,1 +1,15 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import Error from "./error";
+
+let wrapper: ShallowWrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<Error />);
+});
+
+describe("Error Component", () => {
+  it("should have one <h1> tag", () => {
+    expect(wrapper.find("svg")).toHaveLength(1);
+  });
+});
 

9c8c4ac4a255f8c9411243d166243f7a041d383f
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:44:19 +0530

    Update other-myinfo.test.tsx

--- src/shared/components/other-myinfo/other-myinfo.test.tsx
+++ src/shared/components/other-myinfo/other-myinfo.test.tsx
@@ -1,1 +1,28 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import OtherMyinfo from "./other-myinfo";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+
+describe("Other Myinfo Component Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("Other Myinfo component render correctly", () => {
+        store = mockStore({});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <OtherMyinfo/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(OtherMyinfo)).toHaveLength(1);
+      });
+});

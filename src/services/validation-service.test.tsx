a10b24f7b5740659efd684bd9b742fc8447e8ca3
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 07:19:13 +0530

    Update validation-service.test.tsx

--- src/services/validation-service.test.tsx
+++ src/services/validation-service.test.tsx
@@ -1,1 +1,17 @@
+import storeMockData from "../utils/mock/store-spec.json";
+import validateService from "./validation-service";
 
+let store: any;
+
+beforeEach(() => {
+  store = storeMockData.validation;
+});
+
+describe("NRIC Validation", () => {
+  test("Valid NRIC test case", () => {
+    expect(true).toBe(validateService.isValidNRIC(store.validNRIC));
+  });
+  test("Invalid NRIC test case", () => {
+    expect(false).toBe(validateService.isValidNRIC(store.invalidNRIC));
+  });
+});

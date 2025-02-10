8e9321e3d153d7e878c4e0fb85b20b8bf5a5201d
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 09:00:51 +0530

    Update trust-bank-slice.ts

--- src/utils/trust-bank-slice.ts
+++ src/utils/trust-bank-slice.ts
@@ -1,1 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { KeyWithAnyModel } from "./model/common-model";
+const initialState: KeyWithAnyModel = {
+       trustBank : {}
+};
 
+const trustBank = createSlice({
+        name: "trustBank",
+        initialState,
+        reducers: {
+                UpdateTrustBank(state, action) {
+                    state.trustBank = action.payload;
+                }
+        }
+});
+export const trustBankAction = trustBank.actions;
+export default trustBank;

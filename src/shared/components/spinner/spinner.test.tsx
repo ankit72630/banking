7a278277339f27b8002db95f97c498c96850da39
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 11:09:25 +0530

    Update spinner.test.tsx

--- src/shared/components/spinner/spinner.test.tsx
+++ src/shared/components/spinner/spinner.test.tsx
@@ -1,1 +1,31 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import Spinner from "./spinner";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+jest.mock("@lottiefiles/react-lottie-player", () => ({
+  __esModule: true,
+}));
+
+describe("Spinner Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("Spinner component render correctly", () => {
+        store = mockStore({});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <Spinner/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(Spinner)).toHaveLength(1);
+      });
+});

f4e643a8b3dde7d51ad683102d6cd6c3306fffb0
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 08:43:08 +0530

    Update rules_bd-3.ts

--- src/modules/rules/rules_bd-3.ts
+++ src/modules/rules/rules_bd-3.ts
@@ -1,1 +1,23 @@
+import { getUrl } from '../../utils/common/change.utils';
+import { KeyWithAnyModel, ValidationObjModel } from '../../utils/model/common-model';
+import rulesUtils from './rules.utils';
+
+const Rules_bd_3 = (props: KeyWithAnyModel, _application: KeyWithAnyModel): KeyWithAnyModel => {
+    const validationObj: ValidationObjModel = {
+        nonEditable: [],
+        hidden: []
+    };
+    let hiddenFields: Array<string> = [];
+    if (getUrl.getJourneyType() === "ETC") {
+        hiddenFields = ["work_type","name_of_employer","name_of_employer_other","name_of_business","job_title","embossed_name"];
+    }
+    else{
+        hiddenFields = ["credit_limit_consent","myinfo_data_cli","embossed_name"];
+    }
+
+    validationObj.hidden!.push(hiddenFields);
+    return rulesUtils(props, validationObj);
+}
+
+export default Rules_bd_3;
 

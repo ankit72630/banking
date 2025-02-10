93572a9cdf776ee494f4fdbbd51adb8eb3bf6927
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:48:43 +0530

    Update constant.ts

--- src/shared/components/otp-modal/constant.ts
+++ src/shared/components/otp-modal/constant.ts
@@ -1,1 +1,19 @@
+export const constant = {
+    info: "A one-time password has been sent to your phone with the number",
+    resend: "Didn’t receive a one-time password?",
+    expiredOTP: "Expired OTP",
+    expiredTime: "The 6 digits of OTP should be correctly typed within 3 minutes.",
+    newOtp: "Please request a new OTP and try again.",
+    resendBtn: "Resend OTP",
+    issues: "Any other issue?",
+    ccs: "Contact Customer Service",
+    invalidOtp: "Incorrect OTP. Please try again.",
+    expired: "Expired OTP. Please try again.",
+    generated: "OTP Sent Succesfully"
+  }
 
+export const PAYLOAD_CONSTANTS = {
+    flow_type_w8: "W8-BEN",
+    flow_type_card: "CARD",
+    flow_type_cli: "CLI"
+}

4221b3d47b5f10274bf07ef6f211d0eeafc2a4be
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 10:18:08 +0530

    Update document-upload-radio-button.tsx

--- src/shared/components/document-upload/document-upload-radio-button.tsx
+++ src/shared/components/document-upload/document-upload-radio-button.tsx
@@ -1,1 +1,36 @@
+import React from "react";
+export const DocumentUploadRadioButton = (props:any) => {
+  return (
+    <>
+      <div className="document-upload__file-upload">
+        <div className="radioWithLabel">
+          <div key={props.documentList.document_types[0].document_type_code}>
+            <label htmlFor={props.documentList.document_types[0].document_type_code}>
+              <input
+                type="radio"
+                name={props.documentTypes.document_category_code}
+                id={props.documentList.document_types[0].document_type_code}
+                onClick={() => {
+                    props.setCheckedFlag(
+                    props.documentTypes.document_category_code,
+                    props.documentList.document_types[0].document_type_code
+                  );
+                  props.documentList.document_types[0].uploaded_documents = [
+                    {
+                      document_type_code:
+                      props.documentList.document_types[0].document_type_code,
+                      document_status: "UPLOAD",
+                    },
+                  ];
+                }}
+              />
+              <span>{props.documentList.document_types[0].document_type}</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default DocumentUploadRadioButton;

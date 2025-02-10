3a810abe0bf20ac30a64e4912de93edc28daa32a
ankit72630 <76931650+ankit72630@users.noreply.github.com>
2025-02-10 11:14:20 +0530

    Update toggle.test.tsx

--- src/shared/components/toggle/toggle.test.tsx
+++ src/shared/components/toggle/toggle.test.tsx
@@ -1,1 +1,31 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import thunk from "redux-thunk";
+import Toggle from "./toggle";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
+jest.mock("axios", () => ({
+  __esModule: true,
+}));
+jest.mock("@lottiefiles/react-lottie-player", () => ({
+  __esModule: true,
+}));
+
+describe("Toggle component Testing", () => {
+    let wrapper: ShallowWrapper;
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    let store: any;
+
+    test("Toggle component render correctly", () => {
+        store = mockStore({});
+    
+        wrapper = shallow(
+          <Provider store={store}>
+            <Toggle/>
+          </Provider>
+        );
+        expect(wrapper.length).toEqual(1);
+        expect(wrapper.find(Toggle)).toHaveLength(1);
+      });
+});
